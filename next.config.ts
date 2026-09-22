caae9338bdf62797f80378ae3b0127b58b0e33a9
bjbeyond81 <bj_beyond@tutamail.com>
2026-09-22 07:45:21 +0200

    Allow Ultrahuman official CDN images in next/image.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "m.media-amazon.com" },
       { protocol: "https", hostname: "www.apple.com" },
+      { protocol: "https", hostname: "public-web-assets.uh-static.com" },
     ],
   },
 };
